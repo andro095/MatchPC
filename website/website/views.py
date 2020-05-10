c7f23391edf230e21a274491937379b00a6a0d59
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-05-10 00:56:47 -0600

    Renderización del response

--- website/website/views.py
+++ website/website/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
 
 #Funcion vista de la pagina principal
 def index(request):
-    return HttpResponse("Aqui va la pagina principal")
+    return render(request, "index.html", {})
+    #return HttpResponse("Aqui va la pagina principal")
