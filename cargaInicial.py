17a487a0b1d327daba809552776616ffcb197419
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2020-05-09 20:06:12 -0600

    Actualización
    Se añade imagen del grafo y eliminación de un nodo

--- cargaInicial.py
+++ cargaInicial.py
@@ -13,7 +13,7 @@ G=nx.Graph()
 #Lista de nodos: Niveles                                       Ambito   
 G.add_nodes_from(["Universidad", "Diversificado","Profesional", "Diseño Gráfico", "Ing. Industrial"])
 G.add_nodes_from(["User001", "User002", "User003", "User004", "User005"])
-G.add_nodes_from(["AlienWare", "HP Pavillion", "MAC", "Dell Inspiron", "Intel Celeron"])
+G.add_nodes_from(["AlienWare", "HP Pavillion", "MAC", "Intel Celeron"])
 
 #Relaciones de usuarios y campos.(Usuario:PC       Usuario:Niveles         Usuario:Ambito)
 G.add_edges_from([['User001','AlienWare'], ("User001","Universidad"), ("User001","Diseño Gráfico")])
@@ -46,6 +46,6 @@ nx.draw_networkx_edge_labels(G,pos,edge_labels={('User001','AlienWare'):'PC',('U
                                                 ('User004','HP Pavillion'):'PC',('User004','Diversificado'):'Nivel', ('User004','Diseño Gráfico'):'Ambito',\
                                                 ('User005','Intel Celeron'):'PC',('User005','Diversificado'):'Nivel', ('User005','Profesional'):'Ambito',})
 
-#plt.savefig("simple_path.png") #Sirve para guarda la imagen del grafo
+plt.savefig("grafo.png") #Sirve para guarda la imagen del grafo
 plt.axis('off')#No hay punto de referencia
 plt.show() #Mostar grafo
