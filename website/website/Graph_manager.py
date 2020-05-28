a60ccef45d4af634b3511628a60638766af1ebc2
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2020-05-28 08:50:54 -0600

    Actualización de la clase
    Arreglo de bugs, consultas en Graph_manager class

--- website/website/Graph_manager.py
+++ website/website/Graph_manager.py
@@ -1,84 +1,45 @@
 from neo4j import GraphDatabase
 
 #Clase principal
+
 class Graph_manager:
     #Inicialización
-    db = None
     def __init__(self):
         #Conexion y configuracion de python para neo4j
         uri             = "bolt://localhost:7687"
         userName        = "neo4j"
         password        = "123"
-        db = GraphDatabase.driver(uri, auth=("neo4j", "Lisp1234"), encrypted=False)
-        print("simon")
+        self.db = GraphDatabase.driver(uri, auth=(userName, password), encrypted=False)
         
-     #Setter y Getter   
+    #Setter y Getter   
     def get_graph(self):
-        return self.graph
+        return self.db
         
-    def set_graph(self, g):
-        self.graph = g
-        
-    def get_user(self):
-        return self.user
-    
-    def set_user(self, u):
-        self.user = u
-        
-    
+    def set_graph(self, db):
+        self.db = db
         
     #Recomendar computadora a usuario
-    def recommend(self, user, ambito, nivel, pc):
-        N=input("Ingrese Nivel\t")
-        A=input("Ingrese ambito de trabajo\t")
-        
-        with db.session() as session:
-            for record in session.run("MATCH (usuario:user)-[:Nivel|:Ambito|:PC]->(r)"
-                             "WHERE usuario.user = $user "
-                             "RETURN r.pc", ambito=A, nivel=N, pc = pc):
-                print(record["r.pc"])
+    def recommend(self):
+        with self.db.session() as session:
+            record = session.run("MATCH (p:PC),(u:User) WHERE (u)-[:Ambito]->({Ambito:'Diseño Gráfico'}) AND (u)-[:Nivel]->({Level:'Profesional'}) AND (u)-[:PC]->(p) return p")
+            for line in record:
+                print("La computadora que se adecua a su ambito y nivel profesional es:")
+                print (line["p"])
             
-    
-    def add_User(self, user, ambito, nivel, pc):
-        N=input("Ingrese Nivel\t")
-        A=input("Ingrese ambito de trabajo\t")
-        
-        tipo=input("Ingrese tipo de computadora")
-        marca=input("Ingrese marca de computadora")
-        modelo=input("Ingrese modelo de computadora")
-        cpu=input("Ingrese cpu de computadora")
-        tipoRam=input("Ingrese tipo de RAM")
-        capacidadRam=input("Ingrese capacidad de RAM")
-        graficaMarca=input("Ingrese marca de la tarjeta grafica")
-        graficaModelo=input("Ingrese modelo de la tarjeta grafica")
-        
-        
-        
-        with db.session() as session:
-            for record in session.run("CREATE (User{id:id}), Ambito({ambito:A}, Nivel({nivel:N},"
-                                      "PC({tipo:tipo, marca:marca, modelo:modelo, cpu=cpu, tipoRam:tipoRam,"
-                                      "capacidadRam:capacidadRam, graficaMarca:graficaMarca, graficaModelo:graficaModelo})))"):
-                id=id, ambito=A, nivel=N, tipo=tipo, marca=marca, modelo=modelo, cpu=cpu, tipoRam=tipoRam, capacidadRam=capacidadRam, graficaMarca=graficaMarca, graficaModelo=graficaModelo        
+    def add_User(self):
+        g.generate_id
+        with self.db.session() as session:
+            record = session.run("CREATE (a:User { ID: 'User021' }) WITH a "
+"MATCH (a:User),(b:Ambito) WHERE a.ID = 'User021' AND b.Ambito = 'Diseño Gráfico' " 
+"CREATE (a)-[r:Ambito]->(b) WITH a MATCH (a:User), (b:Nivel) WHERE a.ID = 'User019' AND b.Level = 'Profesional' "
+"CREATE (a)-[r:Nivel]->(b) "
+"WITH a, b MATCH (a:User),(b:PC)WHERE a.ID = 'User021' AND b.modelo = 'Alienware Razer' CREATE (a)-[r:PC]->(b)")
+            print("Usuario añadido con exito")
     
     def generate_id(self):
         with db.session() as session:
-            for record in session.run("MATCH (Usuario:user)"
-                             "WHERE a.id = $id"
-                             "RETURN r.id", id=id):
-                print(record["r.id"])
-                id += 1
-                
-manager = Graph_manager()
-Graph_manager.add_User(1,2,3,4,5)
-
-        
-        
-    
-        
+             record = session.run("MATCH (u:User) return COUNT(u)")
             
-            
-        
-    
-
-
-
+g = Graph_manager()
+g.add_User()
+g.recommend()
